91db2ed2d7e2f5edd88411c9340add0885e600bd
Harsh Dalmia <harshdalmia20212081@gmail.com>
2025-01-25 21:17:19 +0530

    togglePassword feature

--- frontend/src/components/Authentication/SignUpPage.jsx
+++ frontend/src/components/Authentication/SignUpPage.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SignUpImg from "./signUPimg.png"
+import { Eye, EyeClosed, Key, KeyRound, Mail } from "lucide-react";
 
 const SignUpPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [togglePassword, setTogglePassword] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,57 +21,58 @@ const SignUpPage = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center min-h-screen bg-white">
       {/* Left Side: Illustration */}
-          <img
-            src={SignUpImg}
-            alt="Illustration"
-            className="w-180 h-128 md:flex mx-auto"
-          />
+      <img
+        src={SignUpImg}
+        alt="Illustration"
+        className="w-180 h-128 md:flex mx-auto"
+      />
 
       <div className="flex items-center justify-center w-full md:w-1/2 h-screen p-10 bg-gray-100">
         <div className="max-w-md w-full">
-        <h1 className="text-5xl font-bold mb-8 flex items-center justify-center gap-2 drop-shadow-lg">Join <p className="text-5xl text-purple-800">$PEND</p> Sense</h1>
+          <h1 className="text-5xl font-bold mb-8 flex items-center justify-center gap-2 drop-shadow-lg">Join <p className="text-5xl text-purple-800">$PEND</p> Sense</h1>
           <h2 className="text-3xl font-bold text-purple-800 text-center mb-6">
             Sign Up
           </h2>
           <form onSubmit={handleSubmit} className="space-y-4">
             {/* Email Input */}
             <div>
-              <label className="block text-gray-700 font-medium">Email</label>
+              <label className="text-gray-700 font-medium flex gap-2 m-1"><Mail className="w-4.5" />Email</label>
               <input
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                placeholder="✉ example@gmail.com"
+                placeholder="example@gmail.com"
                 className="w-full border rounded-md py-2 px-4 focus:outline-none focus:border-purple-500"
                 required
               />
             </div>
 
             {/* Password Input */}
-            <div>
-              <label className="block text-gray-700 font-medium">
-                Password
+            <div className="relative">
+              <label className="gap-2 m-1 text-gray-700 font-medium flex">
+                <KeyRound className="w-4.5" /> Password
               </label>
               <input
-                type="password"
+                type={togglePassword ? "password" : "text"}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                placeholder="🔒 Enter your password"
+                placeholder="Enter your password"
                 className="w-full border rounded-md py-2 px-4 focus:outline-none focus:border-purple-500"
                 required
               />
+              {togglePassword ? <Eye onClick={() => setTogglePassword(!togglePassword)} className="absolute inset-y-9 right-3" /> : <EyeClosed onClick={() => setTogglePassword(!togglePassword)} className="absolute inset-y-9 right-3" />}
             </div>
 
             {/* Confirm Password Input */}
-            <div>
-              <label className="block text-gray-700 font-medium">
-                Confirm Password
+            <div className="relative">
+              <label className="gap-2 m-1 text-gray-700 font-medium flex">
+                <KeyRound className="w-4.5" />Confirm Password
               </label>
               <input
                 type="password"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
-                placeholder="🔒 Confirm your password"
+                placeholder="Confirm your password"
                 className="w-full border rounded-md py-2 px-4 focus:outline-none focus:border-purple-500"
                 required
               />
