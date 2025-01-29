5302fc74d7356aebabb2ab38db36a414450c0a79
Harsh Dalmia <harshdalmia20212081@gmail.com>
2025-01-29 19:00:06 +0530

    completed recent users in admin dashboard

--- frontend/src/components/Admin/RecentUsers.jsx
+++ frontend/src/components/Admin/RecentUsers.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Avatar from 'react-avatar';
 
-
-const recentUsers = [
-    { id: 1, name: "Alice Johnson", email: "alice@example.com", role: "User", joinDate: "2023-07-01" },
-    { id: 2, name: "Bob Smith", email: "bob@example.com", role: "Manager", joinDate: "2023-07-02" },
-    { id: 3, name: "Charlie Brown", email: "charlie@example.com", role: "User", joinDate: "2023-07-03" },
-    { id: 4, name: "Diana Ross", email: "diana@example.com", role: "Admin", joinDate: "2023-07-04" },
-    { id: 5, name: "Edward Norton", email: "edward@example.com", role: "User", joinDate: "2023-07-05" },
-]
-
-const colors=['red','green', 'blue','orange']
-
 const RecentUsers = () => {
+    const [recentUsers, setRecentUsers] = useState([])
+
+    useEffect(() => {
+        const getUsers = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/getUsers');
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+
+                const sortedData = data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+                setRecentUsers(sortedData.slice(0, 5));
+            } catch (error) {
+                console.error("Error fetching users:", error);
+            }
+        };
+
+        getUsers();
+    }, []);
+
+    const formatDate = (isoDateString) => {
+        const date = new Date(isoDateString);
+        const formattedDate = date.toLocaleDateString();
+        return formattedDate
+    }
+
     return (
         <div className='space-y-2 w-1/2 border border-gray-500 rounded-xl p-2 px-4 overflow-auto h-96'>
             <h1 className='px-5 text-2xl font-bold'>Recent Users</h1>
@@ -28,7 +45,7 @@ const RecentUsers = () => {
                     {recentUsers?.map((user, index) => (
                         <tr key={index} className="hover:bg-gray-50">
                             <td className="p-4 border-b border-gray-200 flex items-center space-x-2 ">
-                                <Avatar round={true} color={colors[Math.floor(Math.random() * colors.length)]} name={user.name} size="40" />
+                                <Avatar round={true} name={user.name} size="40" />
                                 <div className='flex flex-col justify-center'>
                                     <span>{user.name}</span>
                                     <span className='text-sm text-gray-600'>
@@ -37,7 +54,7 @@ const RecentUsers = () => {
                                 </div>
                             </td>
                             <td className="p-4 border-b border-gray-200">{user.role}</td>
-                            <td className="p-4 border-b border-gray-200">{user.joinDate}</td>
+                            <td className="p-4 border-b border-gray-200">{formatDate(user.createdAt)}</td>
                         </tr>
                     ))}
                 </tbody>
